09f12cb1abeaf676ff3fa46305320a14396a829d
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:07:54 -0400

    added a list

--- aaa.py
+++ aaa.py
@@ -1,8 +1,17 @@
-a=90.2
-print(a)
-print(id(a))
-print(type(a))
+# a=90.2
+# print(a)
+# print(id(a))
+# print(type(a))
 
-a=a+10
-print(a)
-print(id(a))
+# a=a+10
+# print(a)
+# print(id(a))
+
+b=list()
+print(b)
+print(type(b))
+print(id(b))
+b.append(100)
+print(b)
+print(type(b))
+print(id(b))
