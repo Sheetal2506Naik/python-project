65ddc1ffdc6a9aabd62f47b3c288f464dc7a28d2
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:20:07 -0400

    print age of the user

--- aaa.py
+++ aaa.py
@@ -7,11 +7,7 @@
 # print(a)
 # print(id(a))
 
-b=list()
-print(b)
-print(type(b))
-print(id(b))
-b.append(100)
-print(b)
-print(type(b))
-print(id(b))
+# 
+c=input("enter your age:")
+d=c-10
+print(d)
