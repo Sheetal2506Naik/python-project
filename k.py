50c2866a98b4a2601705895f422271d7a27f0e07
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-30 04:08:49 -0400

    added a file k.py

---
+++ k.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+url="http://whatismyip.host/"
+get_link=requests.get(url,headers={'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"})
+print(get_link.text)
+# soup=BeautifulSoup(get_link.text,'html.parser')
+# ip=soup.find(class_="ipaddress").text
+# print(ip)
+
+
+
+
